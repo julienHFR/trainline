08f37c2a21963f6d2e04d9afa20bb174472770b2
fonky <test@test.com>
2018-06-01 22:19:27 +0100

    added API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
+import axios from 'axios';
 import Services from './components/services/Services';
 
 class App extends React.Component {
   /**
+   * Constructor
+   * @param {*} props
+   */
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, errorString: null };
+  }
+
+  /**
    * Initialize services data into the state
    */
   componentWillMount() {
+    const servicesUrl = 'https://realtime.thetrainline.com/departures/wat';
+    axios
+      .get(servicesUrl)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        this.componentDidCatch(error);
+      });
+  }
+
+  /**
+   * Catch error and put it in the state
+   * @param {*} error
+   */
+  componentDidCatch(error) {
+    this.setState({ hasError: true, errorString: error });
   }
 
   /**
    * Render the application
    */
   render() {
+    if (this.state.hasError) {
+      return <h1>{this.state.errorString.toString()}</h1>;
+    }
     return (
       <div className="main">
         <Services />
