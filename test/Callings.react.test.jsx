01abbf3298dd151f887db5b8665838c36db90a75
fonky <test@test.com>
2018-06-04 20:43:00 +0100

    delete

--- test/Callings.react.test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Callings from '../src/components/callings/Callings';
-
-const component = renderer.create(
-  <Callings />
-);
--- test/Services.react.test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Services from '../src/components/services/Services';
-
-const component = renderer.create(
-  <Services />
-);
